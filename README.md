00ed4b8081c2b33a4675bd296ebc62770eb5560d
Javier Bullrich <javier@bullrich.dev>
2023-10-10 19:32:20 +0200

    added github-action code

---
+++ README.md
@@ -0,0 +1,47 @@
+# github-metrics
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
+
+This project was created using `bun init` in bun v0.5.9. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+## GitHub action
+
+To use, create the file `.github/workflows/metrics.yml` and paste the following:
+
+```yml
+name: Get metrics
+
+on:
+  workflow_dispatch:
+    inputs:
+      ORG:
+        description: 'Organization'
+        required: true
+        type: string
+      REPO:
+        description: 'Repository name'
+        required: true
+        type: string
+  
+jobs:
+  repository-metrics:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Fetch metrics
+        uses: Bullrich/github-metrics@main
+        env:
+          owner: ${{ inputs.ORG }}
+          repo: ${{ inputs.REPO }}
+          # You need a personal access token with scope repo
+          GITHUB_TOKEN: ${{ secrets.PAT }}
+```
---
+++ action.ts
@@ -0,0 +1,50 @@
+import { context, getOctokit } from "@actions/github";
+import { GitHubApi } from "./src/github";
+import { getInput, summary } from "@actions/core";
+
+console.log("Hello via Bun!");
+
+
+const token = getInput("TOKEN", { required: true });
+
+const getRepo = () => {
+    let repo = getInput("repo", { required: false });
+    if (!repo) {
+        repo = context.repo.repo;
+    }
+
+    let owner = getInput("owner", { required: false });
+    if (!owner) {
+        owner = context.repo.owner;
+    }
+
+    return { repo, owner };
+};
+
+const repo = getRepo();
+
+const instance = getOctokit(token);
+
+const client = new GitHubApi(instance, { org: repo.owner, repo: repo.repo });
+
+const total = await client.fetchCount();
+console.log("Total amount", total);
+
+const report = await client.downloadIssues();
+
+summary
+    .addHeading(`Metrics report for ${repo.owner}/${repo.repo}`)
+    .addHeading("Issues", 3)
+    .addTable([
+        ["Total", report.issues.total.toString()],
+        ["Open", report.issues.open.toString()],
+        ["Closed", report.issues.closed.toString()],
+    ]).addRaw(`Average duration: ${report.issues.duration}`)
+    .addEOL()
+    .addHeading("Pull Requests", 3)
+    .addTable([
+        ["Total", report.prs.total.toString()],
+        ["Open", report.prs.open.toString()],
+        ["Closed", report.prs.closed.toString()],
+    ]).addRaw(`Average duration: ${report.prs.duration}`)
+    .write();
